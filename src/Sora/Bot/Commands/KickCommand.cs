5833ffe8790e3538295fb007c8d91898908ec37b
Enjuu <30718372+JustinOsu@users.noreply.github.com>
2021-10-17 23:08:15 +0200

    didn't tried kick... should work idk

--- src/Sora/Bot/Commands/KickCommand.cs
+++ src/Sora/Bot/Commands/KickCommand.cs
@@ -1,3 +1,8 @@
+using Sora.Database;
+using Sora.Database.Models;
+using Sora.Enums;
+using Sora.Packets.Server;
+using Sora.Services;
 using System;
 using System.Collections.Generic;
 using Presence = Sora.Objects.Presence;
@@ -12,6 +17,26 @@ namespace Sora.Bot.Commands
         public int ExpectedArgs => 1;
         public Permission RequiredPermission => Permission.From(Permission.ADMIN_KICK);
 
-        public bool Execute(Presence executer, string[] args) => throw new NotImplementedException();
+        private Sora sora;
+
+        private PresenceService ps;
+
+        public bool Execute(Presence executer, string ch, string[] args)
+        {
+            if (ps.TryGet(args[1], out var pr))
+            {
+                sora.SendMessage("You have been kicked from the server.", args[1], true);
+
+                sora.SendMessage(args[1] + " has been kicked from the server.", executer.User.UserName, true);
+
+                pr.Push(new LoginResponse(LoginResponses.Failed));
+                return true;
+            }
+            else
+            {
+                sora.SendMessage("This user isn't online.", executer.User.UserName, true);
+                return true;
+            }
+        }
     }
 }
